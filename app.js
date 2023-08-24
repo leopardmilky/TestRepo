85fd803d3887b844bedd0de7386d79ed87074de5
leopardmilky <matthew.lee0619@gmail.com>
2023-08-24 18:32:18 +0900

    유효성 검사 진행중

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 // const moment = require('moment');
+const Joi = require('joi'); // Joi => JavaScript 유효성 검사 도구.
 const Board = require('./models/board');
 const Comment = require('./models/comment');
 
@@ -29,58 +32,68 @@ app.get('/', (req, res) => {
     res.send('hello world!');
 });
 
-app.get('/index', async (req, res) => {
+app.get('/index', catchAsync(async (req, res) => {
     const board = await Board.find({});
     res.render('board/index', {contents: board});
-});
+}));
 
 app.get('/index/new', (req, res) => {
     res.render('board/new');
 });
 
-app.post('/index', async (req, res) => {
+app.post('/index', catchAsync(async (req, res) => {
     const board = new Board(req.body.board);
     await board.save();
     res.redirect(`/index/${board._id}`);
-});
+}));
 
-app.get('/index/:id', async (req, res) => {
+app.get('/index/:id', catchAsync(async (req, res) => {
     const board = await Board.findById(req.params.id).populate('comments'); // populate()가 있어야 ref
     // console.log(board);
     res.render('board/show', {items: board});
-});
+}));
 
-app.get('/index/:id/edit', async (req, res) => {
+app.get('/index/:id/edit', catchAsync(async (req, res) => {
     const board = await Board.findById(req.params.id);
     res.render('board/edit', {content: board});
-});
+}));
 
-app.put('/index/:id', async (req, res) => {
+app.put('/index/:id', catchAsync(async (req, res) => {
     const {id} = req.params;
     const board = await Board.findByIdAndUpdate(id, req.body.board); // {...req.body.board} ???
     res.redirect(`/index/${board._id}`);
-});
+}));
 
-app.delete('/index/:id', async (req, res) => {
+app.delete('/index/:id', catchAsync(async (req, res) => {
     const {id} = req.params;
     await Board.findByIdAndDelete(id);
     res.redirect('/index');
-});
+}));
 
-app.post('/index/:id/comments', async (req, res) => {
+app.post('/index/:id/comments', catchAsync(async (req, res) => {
     const board = await Board.findById(req.params.id);
     const comment = new Comment(req.body.comment);
     board.comments.push(comment);
     await comment.save();
     await board.save();
     res.redirect(`/index/${board._id}`);
-});
+}));
 
-app.delete('/index/:id/comments/:commentId', async(req, res) => {
+app.delete('/index/:id/comments/:commentId', catchAsync(async(req, res) => {
     const {id, commentId} = req.params;
     await Board.findByIdAndUpdate(id, {$pull: {comments: commentId}});
     await Comment.findByIdAndDelete(req.params.commentId);
     res.redirect(`/index/${id}`);
+}));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if(!err.message) err.message = 'Oh, Something Went Wrong!!';
+    res.status(statusCode).render('error', {err});
 });
 
 app.listen(3000, () => console.log('PORT 3000....!!'));
