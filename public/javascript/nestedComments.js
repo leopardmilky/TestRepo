475d781f5d928b1cde4395e228addbb315897593
leopardmilky <matthew.lee0619@gmail.com>
2023-09-14 21:05:40 +0900

    답변, 수정 UI  완료

--- public/javascript/nestedComments.js
+++ public/javascript/nestedComments.js
@@ -1,44 +1,104 @@
-document.addEventListener('click', function(event){
-    if(event.target.classList.contains('commentEditBtn')){
-        const commentId = event.target.getAttribute('data-comment-id');
-        console.log(commentId);
-        
-        const parenEle = event.target.parentElement.parentElement;
-        console.log(parenEle);
 
-        event.target.style.display = 'none';
+function closeBox(event) {
 
-        const a = document.createElement('div');
-        const b = document.createElement('label');
-        const c = document.createElement('textarea');
-        const d = document.createElement('div');
-        const e = document.createElement('span');
-        const f = document.createElement('span');
+    const editCommentWrap = document.querySelector('.editCommentWrap');
+    const replyCommentWrap = document.querySelector('.replyCommentWrap');
 
-        a.setAttribute('class', 'mb-3 px-3');
-        b.setAttribute('class', 'form label w-100');
-        c.setAttribute('class', 'form-control');
-        d.setAttribute('class', 'd-flex justify-content-end mt-1');
-        e.setAttribute('class', 'btn btn-outline-warning btn-sm p-1 me-1');
-        e.innerHTML = "등록";
-        f.setAttribute('class', 'btn btn-outline-secondary btn-sm p-1');
-        f.innerHTML = "취소";
+    const findActive = document.querySelectorAll('.active');
 
-        const atag = parenEle.appendChild(a);
-        const btag = atag.appendChild(b);
-        const ctag = atag.appendChild(c);
-        const dtag = atag.appendChild(d);
-        const etag = dtag.appendChild(e);
-        const ftag = dtag.appendChild(f);
-        
+    if(findActive.length > 1){ // 답변/수정 버튼 클릭시 이전에 눌렀던 버튼 비활성화.
+        findActive.forEach(element => {   
+            if(element.attributes[2] != event.target.attributes[2]){
+                element.removeAttribute('aria-pressed');
+                element.removeAttribute('class');
+                element.setAttribute('aria-pressed', false);
+                element.setAttribute('class', 'btn btn-outline-secondary btn-sm p-1 me-1');
+            }
+        });
     }
-})
 
+    if(editCommentWrap){
+        editCommentWrap.remove();
+    }
 
-    // const editbtns = document.querySelectorAll('.editComment');
-    // for(let editbtn of editbtns ){
-    //     editbtn.addEventListener('click', function(){
-    //         const item = this.getAttribute('data-comment-id');
-    //         console.log(item);
-    //     })
-    // }
+    if(replyCommentWrap){
+        replyCommentWrap.remove();
+    }
+}
+
+function commentEditBox(event){
+
+    const commentText = event.target.parentElement.previousElementSibling.firstElementChild.innerHTML;
+    // const commentText = event.target.parentElement.previousElementSibling.children[0].innerHTML;
+    const parentEle = event.target.parentElement.parentElement;
+
+    const a = document.createElement('div');
+    const b = document.createElement('label');
+    const c = document.createElement('textarea');
+    const d = document.createElement('div');
+    const e = document.createElement('span');
+
+    a.setAttribute('class', 'mb-3 px-3 editCommentWrap');
+    b.setAttribute('class', 'form label w-100');
+    c.setAttribute('class', 'form-control');
+    c.innerHTML = commentText;
+    d.setAttribute('class', 'd-flex justify-content-end mt-1');
+    e.setAttribute('class', 'btn btn-outline-secondary btn-sm p-1 me-1');
+    e.innerHTML = "등록";
+
+    const atag = parentEle.appendChild(a);
+    const btag = atag.appendChild(b);
+    const ctag = atag.appendChild(c);
+    const dtag = atag.appendChild(d);
+    const etag = dtag.appendChild(e);
+
+}
+
+function commentReplyBox(event) {
+
+    const parentEle = event.target.parentElement.parentElement;
+
+    const a = document.createElement('div');
+    const b = document.createElement('label');
+    const c = document.createElement('textarea');
+    const d = document.createElement('div');
+    const e = document.createElement('span');
+
+    a.setAttribute('class', 'mb-3 px-3 replyCommentWrap');
+    b.setAttribute('class', 'form label w-100');
+    c.setAttribute('class', 'form-control');
+    d.setAttribute('class', 'd-flex justify-content-end mt-1');
+    e.setAttribute('class', 'btn btn-outline-secondary btn-sm p-1 me-1');
+    e.innerHTML = "등록";
+
+    const atag = parentEle.appendChild(a);
+    const btag = atag.appendChild(b);
+    const ctag = atag.appendChild(c);
+    const dtag = atag.appendChild(d);
+    const etag = dtag.appendChild(e);
+
+}
+
+document.addEventListener('click', function(event){  // 답변,수정 버튼 관련
+
+    const dataCommentEdit =  event.target.getAttribute('data-comment-edit');
+    const dataReplyComment = event.target.getAttribute('data-reply-comment');
+    const sameButton = event.target.classList.contains('active');
+
+    if(dataCommentEdit){
+        closeBox(event);
+        commentEditBox(event);
+        if(!sameButton){ // 같은 버튼 다시 눌렀을때 박스 닫기
+            closeBox();
+        }
+    }
+
+    if(dataReplyComment){
+        closeBox(event);
+        commentReplyBox(event);
+        if(!sameButton){ 
+            closeBox();
+        }
+    }
+
+});
