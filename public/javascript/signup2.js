833c00661905c55b26b279de39d24b3999849f5d
leopardmilky <matthew.lee0619@gmail.com>
2023-11-30 22:26:59 +0900

    css 수정중 (부트스트랩 제거, footer고정)

--- public/javascript/signup2.js
+++ public/javascript/signup2.js
@@ -1,6 +1,6 @@
 // 이메일 중복 체크
 const emailBtn = document.getElementById('emailBtn');
-emailBtn.addEventListener('click', async() => {
+async function checkEmail() {
     const email = document.getElementById('email');
     const emailMsg = document.getElementById('emailMsg');
     const emailRegExp = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/
@@ -34,7 +34,7 @@ emailBtn.addEventListener('click', async() => {
             emailMsg.setAttribute('class', 'invalidEmailMsg');
         }
     }
-})
+}
 
 
 function createVerifyCodePage(email) {
